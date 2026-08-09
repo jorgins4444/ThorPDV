aea591631fab34e824f0fbf77149d6e969870bbc
jorgeestudos7-lang <jorgeestudos7@gmail.com>
2026-08-09 15:20:28 -0300

    style: apply canonical management shell to purchases

--- src/app/dashboard/compras/page.tsx
+++ src/app/dashboard/compras/page.tsx
@@ -1,6 +1,7 @@
 import '../[...slug]/module.css';
 import '../[...slug]/advanced.css';
 import '../[...slug]/purchase.css';
+import '../[...slug]/management-shell.css';
 import { AdvancedShell } from '../[...slug]/advanced-shell';
 import { PurchaseWorkspace } from '../[...slug]/purchase-workspace';
 import { erpLoad } from '../[...slug]/actions';
