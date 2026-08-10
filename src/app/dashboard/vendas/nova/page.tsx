36232a92c16e4ed13a56ebb00098b38dff0e6da3
jorgeestudos7-lang <jorgeestudos7@gmail.com>
2026-08-09 22:41:19 -0300

    feat: load Sales Options in new sale

--- src/app/dashboard/vendas/nova/page.tsx
+++ src/app/dashboard/vendas/nova/page.tsx
@@ -2,12 +2,13 @@ import '../../[...slug]/module.css';
 import '../../[...slug]/advanced.css';
 import '../../[...slug]/sale.css';
 import '../../[...slug]/sales-order.css';
+import '../../[...slug]/sales-order-centralized.css';
 import { AdvancedShell } from '../../[...slug]/advanced-shell';
 import { SaleWorkspaceV070 } from '../../[...slug]/sale-workspace-v070';
 import { erpLoad } from '../../[...slug]/actions';
-import { paymentTermList } from '../../[...slug]/sales-order-actions';
+import { salesOptionsGet } from '../../[...slug]/sales-options-actions';
 
 export default async function NewSalePage(){
-  const [customers,priceTables,terms]=await Promise.all([erpLoad('customers'),erpLoad('price_tables'),paymentTermList()]);
-  return <AdvancedShell title="Nova Venda" subtitle="Venda à vista quitada ou venda a prazo com Boleto/Crediário e parcelas em Contas a Receber." activePath="/dashboard/vendas/nova"><SaleWorkspaceV070 customers={customers.data} priceTables={priceTables.data} terms={terms.data}/></AdvancedShell>;
+  const [customers,priceTables,options]=await Promise.all([erpLoad('customers'),erpLoad('price_tables'),salesOptionsGet()]);
+  return <AdvancedShell title="Nova Venda" subtitle="Venda à vista ou a prazo usando as formas e condições definidas em Opções de Vendas." activePath="/dashboard/vendas/nova"><SaleWorkspaceV070 customers={customers.data} priceTables={priceTables.data} salesOptions={{payment_methods:options.payment_methods,payment_terms:options.payment_terms,card_brands:options.card_brands,card_acquirers:options.card_acquirers,credit_installments:options.credit_installments}}/></AdvancedShell>;
 }
