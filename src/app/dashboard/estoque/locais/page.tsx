e7b8b784edde75f8483da922045e2de82d9d623f
jorgeestudos7-lang <jorgeestudos7@gmail.com>
2026-08-09 23:54:33 -0300

    feat: add stock locations page

---
+++ src/app/dashboard/estoque/locais/page.tsx
@@ -0,0 +1,15 @@
+import '../../[...slug]/module.css';
+import '../../[...slug]/advanced.css';
+import '../stock-home.css';
+import '../stock-locations.css';
+import { AdvancedShell } from '../../[...slug]/advanced-shell';
+import { StockLocationsWorkspace } from '../../[...slug]/stock-locations-workspace';
+import { erpLoad } from '../../[...slug]/actions';
+import { erpStockOverview } from '../../[...slug]/stock-location-actions';
+
+export default async function StockLocationsPage(){
+  const [overview,branches]=await Promise.all([erpStockOverview(),erpLoad('branches')]);
+  return <AdvancedShell title="Locais de Estoque" subtitle="Separe os saldos por Matriz, Filial, Depósito ou qualquer outro local físico." activePath="/dashboard/estoque/locais">
+    <StockLocationsWorkspace locations={overview.locations} branches={branches.data}/>
+  </AdvancedShell>;
+}
