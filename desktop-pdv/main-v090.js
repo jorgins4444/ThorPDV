84c30e2b5bcdafe45675e34982c52503c4a20955
jorgeestudos7-lang <jorgeestudos7@gmail.com>
2026-08-13 12:14:01 -0300

    chore: load product rules

--- desktop-pdv/main-v090.js
+++ desktop-pdv/main-v090.js
@@ -1,2 +1,6 @@
 require('./agent/product-images-v090').installProductImagesV090();
 require('./main.js');
+
+const { ThorAgent } = require('./agent');
+const { Store } = require('./agent/store');
+require('./agent/product-rules-v046').installProductRules(ThorAgent, Store);
