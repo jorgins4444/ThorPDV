b55d502178d9c6da222e11a1ff5d7db4c037d392
jorgeestudos7-lang <jorgeestudos7@gmail.com>
2026-08-15 11:14:27 -0300

    feat: install sale identity and seller support

--- desktop-pdv/main-v090.js
+++ desktop-pdv/main-v090.js
@@ -6,3 +6,4 @@ const { Store } = require('./agent/store');
 require('./agent/product-rules-v046').installProductRules(ThorAgent, Store);
 require('./agent/queue-reconcile-v104').installQueueReconcileV104(Store);
 require('./agent/receipt-print-v829').installReceiptPrintingV829(ThorAgent, Store);
+require('./agent/sale-identity-v830').installSaleIdentityV830(ThorAgent, Store);
