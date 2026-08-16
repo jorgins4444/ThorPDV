055eaa7bad85546f425afcddb8ea123a1e49c856
jorgeestudos7-lang <jorgeestudos7@gmail.com>
2026-08-16 13:11:35 -0300

    feat(pdv): ativar comprovante de suprimento e sangria v106

--- desktop-pdv/main-v090.js
+++ desktop-pdv/main-v090.js
@@ -10,4 +10,6 @@ require('./agent/sale-identity-v830').installSaleIdentityV830(ThorAgent, Store);
 require('./agent/store-credit-return-v105').installStoreCreditReturnV105(ThorAgent, Store);
 require('./agent/return-quantity-guard-v106').installReturnQuantityGuardV106(ThorAgent);
 require('./agent/store-credit-payment-v106').installStoreCreditPaymentV106(ThorAgent, Store);
+require('./agent/cash-movement-receipt-v106').installCashMovementReceiptV106(ThorAgent);
 require('./ipc-store-credit-v105').installStoreCreditVoucherIpcV105(ThorAgent);
+require('./ipc-cash-movement-v106').installCashMovementIpcV106(ThorAgent);
