f4c6923468a2c1d454e1878b708c25cb5e4f086d
jorgeestudos7-lang <jorgeestudos7@gmail.com>
2026-08-16 12:29:12 -0300

    feat: add IPC bridge for store credit vouchers

---
+++ desktop-pdv/ipc-store-credit-v105.js
@@ -0,0 +1,26 @@
+const { ipcMain } = require('electron');
+
+function installStoreCreditVoucherIpcV105(ThorAgent) {
+  if (!ThorAgent.prototype.__storeCreditVoucherInstanceV105) {
+    ThorAgent.prototype.__storeCreditVoucherInstanceV105 = true;
+    const originalStart = ThorAgent.prototype.start;
+    ThorAgent.prototype.start = async function (...args) {
+      global.__thorPdvAgentV105 = this;
+      return originalStart.apply(this, args);
+    };
+  }
+
+  const agent = () => {
+    const instance = global.__thorPdvAgentV105;
+    if (!instance) throw new Error('agent_not_ready');
+    return instance;
+  };
+
+  for (const channel of ['thor:store-credit-voucher', 'thor:print-store-credit-voucher']) {
+    try { ipcMain.removeHandler(channel); } catch {}
+  }
+  ipcMain.handle('thor:store-credit-voucher', async (_event, number) => agent().storeCreditVoucher(number));
+  ipcMain.handle('thor:print-store-credit-voucher', async (_event, voucher) => agent().printStoreCreditVoucher(voucher));
+}
+
+module.exports = { installStoreCreditVoucherIpcV105 };
