b0442a639dee12df3c7705c918bf5be2a6a83c88
jorgeestudos7-lang <jorgeestudos7@gmail.com>
2026-08-17 09:59:35 -0300

    feat(pdv): install crediario receiving agent

--- desktop-pdv/main-v090.js
+++ desktop-pdv/main-v090.js
@@ -12,4 +12,5 @@ require('./agent/return-quantity-guard-v106').installReturnQuantityGuardV106(Tho
 require('./agent/store-credit-payment-v106').installStoreCreditPaymentV106(ThorAgent, Store);
 require('./agent/cash-close-receipt-v112').installCashCloseReceiptV112(ThorAgent);
 require('./agent/cash-close-print-layout-v113').installCashClosePrintLayoutV113(ThorAgent);
+require('./agent/receivable-v115').installReceivableV115(ThorAgent);
 require('./ipc-store-credit-v105').installStoreCreditVoucherIpcV105(ThorAgent);
