bca0b7f0fde54b3531152355348c9adba1467d73
jorgeestudos7-lang <jorgeestudos7@gmail.com>
2026-08-17 09:06:35 -0300

    feat(pdv): install enhanced cash closing receipt

--- desktop-pdv/main-v090.js
+++ desktop-pdv/main-v090.js
@@ -10,4 +10,5 @@ require('./agent/sale-identity-v830').installSaleIdentityV830(ThorAgent, Store);
 require('./agent/store-credit-return-v105').installStoreCreditReturnV105(ThorAgent, Store);
 require('./agent/return-quantity-guard-v106').installReturnQuantityGuardV106(ThorAgent);
 require('./agent/store-credit-payment-v106').installStoreCreditPaymentV106(ThorAgent, Store);
+require('./agent/cash-close-receipt-v112').installCashCloseReceiptV112(ThorAgent);
 require('./ipc-store-credit-v105').installStoreCreditVoucherIpcV105(ThorAgent);
